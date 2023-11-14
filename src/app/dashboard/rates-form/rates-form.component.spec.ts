d7f9caffe3614983366d16cf00774926744d9d5b
Maciej Stojak <maciej.stojak@onwelo.com>
2023-11-14 15:08:31 +0100

    test: fix tests in rates

--- src/app/dashboard/rates-form/rates-form.component.spec.ts
+++ src/app/dashboard/rates-form/rates-form.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RatesFormComponent } from './rates-form.component';
+import { Overlay } from '@angular/cdk/overlay';
+import { FormBuilder } from '@angular/forms';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { DashboardComponent } from '../dashboard.component';
 
 describe('RatesFormComponent', () => {
-  let component: RatesFormComponent;
+  let controller: ApolloTestingController;
+  let dashboard: DashboardComponent;
+  let ratesComponent: RatesFormComponent;
   let fixture: ComponentFixture<RatesFormComponent>;
+  let dashboardFixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RatesFormComponent ]
+      declarations: [ RatesFormComponent, DashboardComponent ],
+      imports: [ApolloTestingModule],
+      providers: [NzNotificationService, Overlay, FormBuilder]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RatesFormComponent);
-    component = fixture.componentInstance;
+    ratesComponent = fixture.componentInstance;
+    controller = TestBed.inject(ApolloTestingController);
+    dashboardFixture = TestBed.createComponent(DashboardComponent);
+    dashboard = dashboardFixture.componentInstance;
+    dashboard.openForm("experience");
+    ratesComponent.person = {
+      id: "MrGreen",
+      name: "Ralph",
+      surname: "Green",
+      departments: [],
+      experiences: [],
+      projects: [],
+      rates: [],
+      roles: [],
+      skills: [],
+    }
+    dashboardFixture.detectChanges();
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    if(!dashboard || !dashboard.currentForm){
+      expect(ratesComponent).toBeFalsy();
+    } else {
+      expect(ratesComponent).toBeTruthy();
+    }
   });
 });
