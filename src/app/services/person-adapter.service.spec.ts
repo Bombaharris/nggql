429c58c5ff92589effef2d051290ec26f6412634
Maciej Stojak <maciej.stojak@onwelo.com>
2023-11-27 10:54:13 +0100

    test: test for person adapter

--- src/app/services/person-adapter.service.spec.ts
+++ src/app/services/person-adapter.service.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PersonAdapterService } from './person-adapter.service';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('PersonAdapterService', () => {
   let service: PersonAdapterService;
+  let controller: ApolloTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [PersonAdapterService],
+      imports: [ApolloTestingModule],
+    });
     service = TestBed.inject(PersonAdapterService);
+    controller = TestBed.inject(ApolloTestingController);
+  });
+
+  afterEach(() => {
+    controller.verify();
   });
 
   it('should be created', () => {
